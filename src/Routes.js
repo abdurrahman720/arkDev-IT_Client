955f71b05326af5955e86d9ef10a411c62f3a80e
abdurrahman720 <personal.abdurrahman@gmail.com>
2022-12-27 23:36:30 +0600

    faq and blog page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./layouts/Root";
+import { Blog } from "./Pages/Blog";
+
 import Courses from "./Pages/Courses";
+import Faq from "./Pages/FAQs";
+
 import Home from "./Pages/Home";
 
 export const router = createBrowserRouter([
@@ -21,6 +25,14 @@ export const router = createBrowserRouter([
                 path: '/courses',
                 element: <Courses></Courses>
 
+            },
+            {
+                path: '/faqs',
+                element: <Faq></Faq>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
     }
